f161c065c3558d35cd2590665cac9483eaf3c815
Tomotada Saigusa <nirot1r@g-second.net>
2019-06-07 23:24:00 +0900

    エンドポイント追加

--- api/index.js
+++ api/index.js
@@ -7,8 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 import devicesRoute from './routes/devices'
+import queuesRoute from './routes/queues'
 
 app.use('/devices', devicesRoute)
+app.use('/queues', queuesRoute)
 
 app.get('/', (req, res) => {
   return res.json({ message: 'hello world!' })
