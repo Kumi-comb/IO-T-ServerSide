b3265647fecebde10085f7209d43116d56944a37
Tomotada Saigusa <nirot1r@g-second.net>
2019-06-08 02:15:51 +0900

    動作確認用

--- view/pages/index.vue
+++ view/pages/index.vue
@@ -1,10 +1,28 @@
 <template lang="pug">
   section
     span I/O-T Server Side
+    button(@click='toggleOn("1")') ON
+    button(@click='toggleOff("1")') OFF
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  methods: {
+    toggleOn: deviceId => {
+      axios.put(`/api/devices/${deviceId}/queue`, {
+        type: "TOGGLE",
+        value: "ON"
+      });
+    },
+    toggleOff: deviceId => {
+      axios.put(`/api/devices/${deviceId}/queue`, {
+        type: "TOGGLE",
+        value: "OFF"
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss">
