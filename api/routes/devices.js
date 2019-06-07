1a3a3d7f8930988b87158b9165444e9397ea0e0c
Tomotada Saigusa <nirot1r@g-second.net>
2019-06-07 14:53:00 +0900

    モック作成 : キュー追加

---
+++ api/routes/devices.js
@@ -0,0 +1,58 @@
+import * as express from 'express'
+import { check, validationResult } from 'express-validator/check'
+
+const router = express.Router()
+
+/**
+ * デバイスキューを追加する
+ */
+router.put(
+  '/:deviceId/queue',
+  [
+    /* キュータイプ(req.body.type) */
+    check('type')
+      .isString()
+      .not()
+      .isEmpty(),
+
+    /* リクエスト状態(req.body.value) */
+    check('value')
+      .isString()
+      .not()
+      .isEmpty()
+  ],
+  (req, res) => {
+    /* バリデーション */
+    const validationErrors = validationResult(req)
+    if (validationErrors.array().length !== 0) {
+      return res
+        .status(422)
+        .json({ status: false, errors: validationErrors.array() })
+    }
+
+    return res.json({
+      id: '1145141919810',
+      type: 'TOGGLE',
+      value: 'ON',
+      status: 'PENDING',
+      user: { id: '11451491910' },
+      timestamp: '1145141919810'
+    })
+  }
+)
+
+/**
+ * デバイスの最新キューを取得する
+ */
+router.get('/:deviceId/queue', (req, res) => {
+  return res.json({
+    id: '1145141919810',
+    type: 'TOGGLE',
+    value: 'ON',
+    status: 'PENDING',
+    user: { id: '11451491910' },
+    timestamp: '1145141919810'
+  })
+})
+
+export default router
