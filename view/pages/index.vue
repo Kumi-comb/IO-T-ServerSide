e0c902571093bd787986382b1cea3f06b9413bbf
Tomotada Saigusa <nirot1r@g-second.net>
2019-06-08 23:54:04 +0900

    角丸にした

--- view/pages/index.vue
+++ view/pages/index.vue
@@ -3,13 +3,23 @@
     .panel
       .panel_header
         span.label コンセント
-        span.label ON
+        span.label.label-on ON
         | 部屋の電灯
       .panel_content.contentWrap
         .contentWrap_item
-          button(@click='toggleOn("1")').btn.btn-block OFF
+          button(@click='toggleOn("1")').btn.btn-block.btn-off OFF
         .contentWrap_item
-          button(@click='toggleOff("1")').btn.btn-block ON
+          button(@click='toggleOff("1")').btn.btn-block.btn-on ON
+    .panel
+      .panel_header
+        span.label コンセント
+        span.label.label-on ON
+        | 部屋の電灯
+      .panel_content.contentWrap
+        .contentWrap_item
+          button(@click='toggleOn("1")').btn.btn-block.btn-off OFF
+        .contentWrap_item
+          button(@click='toggleOff("1")').btn.btn-block.btn-on ON
     //- .modalWrap
     //-   .modal
 </template>
@@ -43,6 +53,7 @@ export default {
 
   background-color: rgba(0, 0, 0, 0.25);
 }
+
 .contentWrap {
   font-size: 0;
 
@@ -55,11 +66,17 @@ export default {
     font-size: 1rem;
   }
 }
+
 .panel {
-  padding: 10px;
+  margin: 10px;
+
+  background-color: #e7e7e7;
+
+  border-bottom: 2px solid #cacaca;
+  border-radius: 10px;
 
   &:not(:last-child) {
-    margin-bottom: 10px;
+    margin-bottom: 20px;
   }
 
   &_header {
@@ -67,23 +84,27 @@ export default {
 
     background-color: #333333;
     color: #ffffff;
+    border-bottom: 2px solid #494949;
+    border-radius: 10px;
   }
 
   &_content {
     padding: 10px;
-
-    background-color: #e7e7e7;
   }
 }
+
 .btn {
   padding: 10px;
 
-  background-color: #302f2f;
-  color: #ffffff;
+  border-radius: 10px;
 
+  background-color: #2f2f2f;
+  color: #ffffff;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25);
   font-size: 1.5em;
 
   border: none;
+  cursor: pointer;
 
   &:active {
     background-color: #727272;
@@ -96,6 +117,42 @@ export default {
   &-block {
     display: block;
     width: 100%;
+  }
+
+  &-off {
+    background-color: #22a543;
+    &:active {
+      background-color: #1a7e33;
+    }
+  }
+  &-on {
+    background-color: #a52222;
+    &:active {
+      background-color: #7e1a1a;
+    }
+  }
+}
+
+.label {
+  display: inline-block;
+
+  padding: 5px 10px;
+  margin-right: 10px;
+
+  vertical-align: middle;
+
+  background-color: #cccccc;
+  color: #333333;
+
+  border-radius: 10px;
+
+  &-off {
+    background-color: #43c05e;
+    color: #ffffff;
+  }
+  &-on {
+    background-color: #c04343;
+    color: #ffffff;
   }
 }
 </style>
